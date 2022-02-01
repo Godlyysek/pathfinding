7a27bc9ed6db516dd190108c20bc435c082c3fd6
Godlyysek <lukynkob@gmail.com>
2022-02-01 23:29:05 +0100

    Added Grid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import Button from './components/Button'
+import Grid from './components/Grid'
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
+      
       <header className="header">
-        
-      <Button text='Choose Algorithm'/>
-      <Button text='Start Node'/>
-      <Button text='End Node'/>
-      <Button text='Start Pathfinding'/>
-        
+          
+        <Button text='Choose Algorithm' id='0'/>
+        <Button text='Start Node' id='1'/>
+        <Button text='End Node' id='2'/>
+        <Button text='Start Pathfinding' id='3'/>
+          
       </header>
+      <div>
+        <Grid row={0}/>
+        <Grid row={1}/>
+        <Grid row={2}/>
+        <Grid row={3}/>
+        <Grid row={4}/>
+        <Grid row={5}/>
+        <Grid row={6}/>
+        <Grid row={7}/>
+        <Grid row={8}/>
+        <Grid row={9}/>
+      </div>
       
     </div>
   );
---
+++ src/components/Grid.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Grid extends Component {
+
+    
+
+  render() {
+    
+    let arr = [];
+    
+    for (let i = (this.props.row * 30); i < (this.props.row * 30 + 30); i++) {
+        arr.push( <div className='block' id={i.toString()}  onClick="select(this.id)">{(i + 1).toString()}</div> )
+    }
+    
+    function select(id){
+        
+    }
+    
+    
+
+
+    return (
+        <div>
+            {arr}
+        </div>
+    );
+  }
+}
